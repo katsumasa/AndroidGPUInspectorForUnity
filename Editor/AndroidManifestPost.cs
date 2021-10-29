2c4483c96f5534270926a0f3574d6f9c06d8b5f9
katsumasa <katsumasa@unity3d.com>
2021-10-29 12:46:42 +0900

    AndroidManifest.xmlを書き換える方向にパッケージの内容を変更

---
+++ Editor/AndroidManifestPost.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEditor.Android;
+using UnityEditor.Build;
+using UTJ.Android.Extensions;
+
+namespace UTJ.Android
+{
+    public class AndroidManifestPost : IPostGenerateGradleAndroidProject
+    {
+
+        public static int prio = 0;
+
+        int IOrderedCallback.callbackOrder
+        {
+            // 取り合えず先頭で
+            get { return prio; }
+        }
+
+
+        void IPostGenerateGradleAndroidProject.OnPostGenerateGradleAndroidProject(string path)
+        {            
+            path = Path.Combine(path,"src","main", "AndroidManifest.xml");
+            var androidManifest = new AndroidManifestReflection(path);
+            androidManifest.SetDebuggableActivity("com.unity3d.player.UnityPlayerActivity", true);
+            androidManifest.AddApplicationMetaDataAttribute("com.android.graphics.developerdriver.enable", "true");
+            androidManifest.Save();
+        }
+    }
+}
